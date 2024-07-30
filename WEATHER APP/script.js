c2c863aaa1f10147c10eac23e94dd78b9943013b
VaidehiRewale <139336081+VaidehiRewale@users.noreply.github.com>
2024-07-30 19:43:08 +0530

    Update script.js

--- WEATHER APP/script.js
+++ WEATHER APP/script.js
@@ -10,13 +10,11 @@ const location_not_found = document.querySelector('.location-not-found');
 
 const weather_body = document.querySelector('.weather-body');
 
-
 async function checkWeather(city){
     const api_key = "0b65d0121412202d3da4a20912035f7d";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
     const weather_data = await fetch(`${url}`).then(response => response.json());
-
 
     if(weather_data.cod === `404`){
         location_not_found.style.display = "flex";
@@ -34,29 +32,26 @@ async function checkWeather(city){
     humidity.innerHTML = `${weather_data.main.humidity}%`;
     wind_speed.innerHTML = `${weather_data.wind.speed}Km/H`;
 
-
     switch(weather_data.weather[0].main){
         case 'Clouds':
-            weather_img.src = "C:/Users/Vaidehi Rewale/Desktop/WEATHER APP/assets/cloud.jpeg";
+            weather_img.src = "assets/cloud.jpeg";
             break;
         case 'Clear':
-            weather_img.src = "C:/Users/Vaidehi Rewale/Desktop/WEATHER APP/assets/clear.jpeg";
+            weather_img.src = "assets/clear.jpeg";
             break;
         case 'Rain':
-            weather_img.src = "C:/Users/Vaidehi Rewale/Desktop/WEATHER APP/assets/rain.jpeg";
+            weather_img.src = "assets/rain.jpeg";
             break;
         case 'Mist':
-            weather_img.src = "C:/Users/Vaidehi Rewale/Desktop/WEATHER APP/assets/mist.jpeg";
+            weather_img.src = "assets/mist.jpeg";
             break;
         case 'Snow':
-            weather_img.src = "C:/Users/Vaidehi Rewale/Desktop/WEATHER APP/assets/snow.jpeg";
+            weather_img.src = "assets/snow.jpeg";
             break;
-
     }
 
     console.log(weather_data);   
 }
-
 
 searchBtn.addEventListener('click', ()=>{
     checkWeather(inputBox.value);
